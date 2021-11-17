e950f813360f4fafc9047c18f8566e6090eeb0eb
moonii <zinta@naver.com>
2021-11-17 14:22:35 +0900

    check expire_at of token

--- src/main/java/org/openpaas/servicebroker/glusterfs/common/ConstantsAuthToken.java
+++ src/main/java/org/openpaas/servicebroker/glusterfs/common/ConstantsAuthToken.java
@@ -1,6 +1,15 @@
 package org.openpaas.servicebroker.glusterfs.common;
 
+import org.springframework.util.StringUtils;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class ConstantsAuthToken {
 
 	public static String AUTH_TOKEN = "";
+	public static String GLUSTERFS_TIMEZONE = "UTC";
+
+	public static ZonedDateTime ISSUED_AT;
+	public static ZonedDateTime EXPIRES_AT;
 }
